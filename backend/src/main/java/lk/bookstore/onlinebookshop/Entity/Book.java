9a4149e66bfb2c66f021e7dc7d68b7ede9c97bc2
nvndu <naviduranjuka@gmail.com>
2025-07-15 00:27:32 +0530

    all crud operations completed

--- backend/src/main/java/lk/bookstore/onlinebookshop/Entity/Book.java
+++ backend/src/main/java/lk/bookstore/onlinebookshop/Entity/Book.java
@@ -1,7 +1,13 @@
 package lk.bookstore.onlinebookshop.Entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
 @Entity(name = "book")
 public class Book {
     @Id
@@ -9,7 +15,7 @@ public class Book {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String ISBN;
+    private String isbn;
 
     @Column(nullable = false, name = "book_name")
     private String name;
@@ -19,4 +25,44 @@ public class Book {
 
     @Column(nullable = false)
     private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
